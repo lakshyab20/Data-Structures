fcd8c9e78f8980df7a389e99490cc118e6cfd106
Lakshya Bhardwaj <47449860+lakshyab20@users.noreply.github.com>
2020-05-31 19:35:33 +0530

    Created a simple Linked List

---
+++ Linked List/LinkedList1.java
@@ -0,0 +1,31 @@
+class LinkedList1{
+	Node head;
+	
+	static class Node{
+		int data;
+		Node next;
+		Node (int d){
+			data=d;
+			next=null;
+		}
+	}
+	
+	public void printList(){
+		Node n = head;
+		while(n!=null){
+			System.out.print(n.data+" ");
+			n=n.next;
+		}
+	}
+	
+	public static void main(String args[]){
+		LinkedList1 list=new LinkedList1();
+		list.head=new Node(1);
+		Node second=new Node(2);
+		Node third=new Node(3);
+		list.head.next=second;
+		second.next=third;
+		list.printList();
+		
+	}
+}
